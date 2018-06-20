8b4ce219d39dc13db30c958666eb30f249bcc08c
Grace Mugoiri <gracemugoiri@gmail.com>
2018-06-20 10:06:39 +0300

    modified the app and index file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """import necessary modules"""
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 
@@ -9,8 +10,9 @@ db = yaml.load(open("db.yaml"))
 app.config["MySQL_HOST"] = db["mysql_host"]
 app.config["MySQL_USER"] = db["mysql_user"]
 app.config["MySQL_PASSWORD"] = db["mysql_password"]
-app.config["MySQL_DB"] = db["mydql_db"]
+app.config["MySQL_DB"] = db["mysql_db"]
 
+mysql = MySQL(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,11 +20,24 @@ def index():
     if request.method == "POST":
         # fetch the data
        userDetails = request.form
-       firstName = userDetails["firstName"]
-       secondName = userDetails["secondName"]
-       foodToEat = userDetails["foodToEat"]
-
+       firstname = userDetails["firstname"]
+       secondname = userDetails["secondname"]
+       foodtoeat = userDetails["foodtoeat"]
+       cur = mysql.connection.cursor()
+       cur.execute("INSERT INTO users(firstname, secondname, foodtoeat) VALUES(%s, %s, %s)",(firstname, secondname, foodtoeat))
+       mysql.connection.commit()
+       cur.close()
+       return redirect("/users")
     return render_template("index.html")
+
+@app.route("/users")
+def users():
+    """define the users function endpoint"""
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template("users.html", userDetails=userDetails)
 
 if __name__ == "__main__":
     app.run(debug=True)
